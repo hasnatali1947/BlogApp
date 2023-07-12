d47b72e676426a70c988c6e441a8790282596175
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-07-12 22:14:41 +0500

    use postgresql instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_12_141430) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "post_id"
+    t.bigint "author_id"
+    t.bigint "post_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,8 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_141430) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "post_id"
+    t.bigint "author_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
@@ -31,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_141430) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
+    t.bigint "author_id"
     t.string "title"
     t.text "text"
     t.integer "comments_counter"
