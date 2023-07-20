9666c6c20fa047a43e34371c659dffe700cba6a0
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-07-20 23:29:23 +0500

    work on views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    @comments = Comment.where(post_id: @posts.pluck(:id)).includes(:user)
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @comments = Comment.where(post_id: @post.id).includes(:user)
+    @users = User.all
+    @user = @post.author
+  end
 end
