85483dca995a705b72746c41a920ea53d21591fc
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-07-21 15:42:36 +0500

    Work on form

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+    def new
+      @post = Post.find(params[:id])
+      @comment = Comment.new
+    end
+  
+    def create
+      @comment = current_user.comments.build(comment_params)
+      @current_post = Post.find(params[:post_id])
+      @post = Post.find(params[:comment][:post_id])
+      if @comment.save
+        redirect_to user_post_path(@post.author, @post)
+        @comment.increment_post_comment_counter
+      else
+        render 'new'
+      end
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:author_id, :post_id, :text)
+    end
+    # ...
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,14 @@ Rails.application.routes.draw do
     get '/users/:id'               => 'users#show'
     get '/users/:user_id/posts'     => 'posts#index'
     get '/users/:user_id/posts/:id' => 'posts#show'
+    get '/posts/new'                => 'posts#new'
+    get '/users/:id/posts/:id/comments/new', to: 'comments#new'
+
+    resources :users do
+        resources :posts do 
+            resources :comments
+            resources :likes
+        end
+    end
 end
+
