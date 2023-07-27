6cc9413b27db4fe62c7f565a78f3d51f97114862
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-07-27 18:20:18 +0500

    added spec and fix n +1 problems

--- spec/intergration/user_show_spec.rb
+++ spec/intergration/user_show_spec.rb
@@ -5,44 +5,53 @@ RSpec.describe User, type: :feature do
   before { subject.save }
 
   describe 'User show page' do
-    it "I can see the user's profile picture." do
+    
+    before do
+      # Create some posts for the user
+      Post.create(author: subject, title: 'Hello', text: 'Hello World')
+      Post.create(author: subject, title: 'World of tech', text: 'Technology is amazing')
+      Post.create(author: subject, title: 'Coding is fun', text: 'I love coding')
       visit user_path(subject.id)
+    end
+
+    it "I can see the user's profile picture." do
       expect(page).to have_css('img')
     end
+
     it "I can see the user's username." do
-      visit user_path(subject.id)
       expect(page).to have_content(subject.name)
     end
+
     it "I can see the number of posts the user has written." do
-      visit user_path(subject.id)
-      expect(page).to have_content(subject.post_counter)
+      expect(page).to have_content('Number of Posts: 3')
     end
+
     it "I can see the user's bio." do
       visit user_path(subject.id)
       expect(page).to have_content(subject.bio)
     end
-    # it "I can see the user's first 3 posts." do
-    #   Post.create([{ author: subject, title: 'First Post', text: 'My first post' },
-    #                { author: subject, title: 'Second Post', text: 'My Second post' },
-    #                { author: subject, title: 'Third Post', text: 'My Third post' }])
-    #   visit user_path(subject.id)
-    #   expect(page).to have_content('first post')
-    #   expect(page).to have_content('second post')
-    #   expect(page).to have_content('third post')
-    # end
+
+
+    it 'Displays the users first 3 posts.' do
+      expect(page).to have_content('Hello')
+      expect(page).to have_content('World of tech')
+      expect(page).to have_content('Coding is fun')
+    end
+
+
     it "I can see a button that lets me view all of a user's posts." do
         visit user_path(subject.id)
         expect(page).to have_link('See all Post', href: user_posts_path(subject))
       end
       
-      it "When I click a user's post, it redirects me to that post's show page." do
-        post = Post.create(author: subject, title: 'First Post', text: 'First post')
+      it 'redirects to the post show page when clicking on a post' do
+        # Create a post for the user
+        post = Post.create(author: subject, title: 'This is my first post', text: 'Some content')
         visit user_path(subject.id)
-        click_on 'First Post'
-        visit user_post_path(subject.id, post.id)
-        expect(page).to have_content('First Post')
+        click_link 'This is my first post', wait: 10
+        expect(page).to have_content('This is my first post')
       end
-      
+    
       
       
       
